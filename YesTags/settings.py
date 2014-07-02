3512f11f2929cc74f8aece819090226a79202d6d
Jared Martin <jaredtmartin@gmail.com>
2014-07-02 19:29:12 -0400

    adjusted settings to fit mysql on orange server

--- YesTags/settings.py
+++ YesTags/settings.py
@@ -14,7 +14,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'yestags',                      # Or path to database file if using sqlite3.
-        'USER': 'jaredthane',                      # Not used with sqlite3.
+        'USER': 'root',                      # Not used with sqlite3.
         'PASSWORD': 't1bur0n',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
